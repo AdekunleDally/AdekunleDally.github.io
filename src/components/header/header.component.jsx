87ac6630f8c3e0895946a84d77a4c14ca115f551
ADEOKUN LUKMON <adekunledally@gmail.com>
2021-01-29 22:49:28 -0800

    Re-write Homepage of the website to be responsive

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,11 +5,13 @@ import {ReactComponent as Logo} from '../../assets/L5.svg'
 
 const Header=()=>(
     <div className="header">
-      <Logo  className="logo"/>
-      <div className="nav-links">
-        <Link className="nav-link" to='/About'>About Me</Link>
-        <Link className="nav-link" to='/MySkills'>My Skills</Link>
-        <Link className="nav-link" to='/Projects'>Projects</Link>
+      <div className="logo-container">
+        <Logo  className="logo" />
+      </div>
+        <div className="nav-links">
+          <Link className="nav-link" to='/About'>About Me</Link>
+          <Link className="nav-link" to='/MySkills'>My Skills</Link>
+         <Link className="nav-link" to='/Projects'>Projects</Link>
         </div>
     </div>
 )
